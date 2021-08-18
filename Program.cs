2772f8f750400f3ede233112ffa9e809fb796837
Nick Stockton <phoenix76728@gmail.com>
2021-08-18 14:45:39 -0400

    hello world mod

--- Program.cs
+++ Program.cs
@@ -9,7 +9,8 @@ namespace HelloWorld
             Console.WriteLine("What is your Name?");
             string use = Console.ReadLine();
             Console.WriteLine("And what is your age?");
-            int age = Console.ReadLine();
+            string use2 = Console.ReadLine();
+            int age = Convert.ToInt32(use2);
 
             Console.WriteLine("Nice to meet you " + use + "and you look way younger than " +age);
             
